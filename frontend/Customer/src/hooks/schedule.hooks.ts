3833b89e49c8e9b310acf7f551510f24b98d8804
Izzanauzi <izzanauzi17@gmail.com>
2026-05-15 21:45:19 +0700

    fix: koneksi api booking customer frontend

--- frontend/Customer/src/hooks/schedule.hooks.ts
+++ frontend/Customer/src/hooks/schedule.hooks.ts
@@ -12,7 +12,7 @@ export function useGetSchedule() {
     queryFn: () =>
       baseFetch<ScheduleResponse>({
         method: "GET",
-        url: "/api/v1/schedule",
+        url: "/schedule",
         options: { showError: false },
       }),
 
